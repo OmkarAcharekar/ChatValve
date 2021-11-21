35d9c37189ee7768f3422af702a678cc23d5b9e5
Omkar Acharekar <omkaracharekar12@gmail.com>
2021-11-21 12:14:17 +0530

    User & NewChatRoom

--- components/UserItem/UserItem.tsx
+++ components/UserItem/UserItem.tsx
@@ -1,14 +1,33 @@
 import { useNavigation } from '@react-navigation/native';
 import  React from 'react';
 import {Image, Text,Pressable ,StyleSheet,View} from 'react-native';
-
+import { Auth, DataStore } from "aws-amplify";
+import { ChatRoom } from '../../src/models';
 
 import styles from './styles';
+import { User,ChatRoomUser } from '../../src/models';
 
 export default function  UserItem({user}) {
  
   const navigation = useNavigation();
-  const onPress = () =>{
+  const onPress = async() => {
+
+    const newChatRoom = await DataStore.save(new ChatRoom({newMessages : 0}));
+
+    const authUser  = await Auth.currentAuthenticatedUser();
+    const dbUser = await DataStore.query(User,authUser.attributes.sub);
+    console.log(dbUser);
+    await DataStore.save(new ChatRoomUser({
+      user:dbUser,
+      chatroom:newChatRoom
+    }))
+   
+    await DataStore.save(new ChatRoomUser({
+      user,
+      chatroom:newChatRoom
+    }))
+
+    navigation.navigate('ChatRoom',{ id: newChatRoom.id});
 
    
    
--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -1,14 +1,28 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import {StyleSheet,View,FlatList} from 'react-native';
 import UserItem from '../components/UserItem';
 
-import Users from '../assets/dummy-data/Users';
+// import Users from '../assets/dummy-data/Users';
 
-
+import { Auth, DataStore } from "aws-amplify";
+import { ChatRoom, User, ChatRoomUser } from "../src/models";
 export default function UsersScreen() {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    DataStore.query(User).then(setUsers);
+  }, []);
+  // useEffect(() => {
+  //  const fetchUsers = async() =>{
+  //    const fetchedUsers = await DataStore.query(User);
+  //     setUsers(fetchedUsers);
+  //  };
+  //  fetchUsers();
+  // }, []);
+
   return (
     <View style = {styles.page}>
-     <FlatList data = {Users}
+     <FlatList data = {users}
      renderItem ={({item}) => <UserItem user ={item} />}
      showsVerticalScrollIndicator ={false}
      />

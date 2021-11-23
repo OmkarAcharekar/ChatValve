f906ba3530c1a2b7b7663ddc52332c1194977c6e
Omkar Acharekar <omkaracharekar12@gmail.com>
2021-11-23 11:39:56 +0530

    check

--- components/UserItem/UserItem.tsx
+++ components/UserItem/UserItem.tsx
@@ -1,39 +1,19 @@
 import { useNavigation } from '@react-navigation/native';
 import  React from 'react';
 import {Image, Text,Pressable ,StyleSheet,View} from 'react-native';
-import { Auth, DataStore } from "aws-amplify";
-import { ChatRoom } from '../../src/models';
+import { Feather } from "@expo/vector-icons";
 
 import styles from './styles';
 import { User,ChatRoomUser } from '../../src/models';
 
-export default function  UserItem({user}) {
+export default function  UserItem({ user,
+  onPress,
  
-  const navigation = useNavigation();
-  const onPress = async() => {
-
-    const newChatRoom = await DataStore.save(new ChatRoom({newMessages : 0}));
-
-    const authUser  = await Auth.currentAuthenticatedUser();
-    const dbUser = await DataStore.query(User,authUser.attributes.sub);
-    console.log(dbUser);
-    await DataStore.save(new ChatRoomUser({
-      user:dbUser,
-      chatroom:newChatRoom
-    }))
-   
-    await DataStore.save(new ChatRoomUser({
-      user,
-      chatroom:newChatRoom
-    }))
-
-    navigation.navigate('ChatRoom',{ id: newChatRoom.id});
-
-   
-   
-
-  }
+  isSelected,
+  isAdmin = false,}) {
+ 
   
+
   return (
     <Pressable onPress={onPress} style = {styles.container}>
     <Image source ={{
@@ -48,6 +28,13 @@ export default function  UserItem({user}) {
       </View>
      
     </View>
+    {isSelected !== undefined && (
+        <Feather
+          name={isSelected ? "check-circle" : "circle"}
+          size={20}
+          color="#4f4f4f"
+        />
+      )}
 
   </Pressable>
   );
--- screens/UsersScreen.tsx
+++ screens/UsersScreen.tsx
@@ -1,17 +1,76 @@
 import React, { useState, useEffect } from 'react';
-import {StyleSheet,View,FlatList} from 'react-native';
+import {StyleSheet,View,FlatList,Text,Pressable,SafeAreaView} from 'react-native';
 import UserItem from '../components/UserItem';
 
 // import Users from '../assets/dummy-data/Users';
 import { Auth, DataStore } from "aws-amplify";
-
+import NewGroupButton from '../components/NewGroupButton';
 import { ChatRoom, User, ChatRoomUser } from "../src/models";
+import { useNavigation } from '@react-navigation/native';
 export default function UsersScreen() {
   const [users, setUsers] = useState<User[]>([]);
-
+  const navigation = useNavigation();
+  const [isNewGroup, setIsNewGroup] = useState(false);
+  const [selectedUsers, setSelectedUsers] = useState<User[]>([]);
   useEffect(() => {
     DataStore.query(User).then(setUsers);
   }, []);
+
+
+  const addUserToChatRoom = async (user, chatroom) => {
+    DataStore.save(new ChatRoomUser({ user, chatroom }));
+  };
+  const isUserSelected = (user) => {
+    return selectedUsers.some((selectedUser) => selectedUser.id === user.id);
+  };
+  const onUserPress = async (user) => {
+    if (isNewGroup) {
+      if (isUserSelected(user)) {
+        // remove it from selected
+        setSelectedUsers(
+          selectedUsers.filter((selectedUser) => selectedUser.id !== user.id)
+        );
+      } else {
+        setSelectedUsers([...selectedUsers, user]);
+      }
+    } else {
+      await createChatRoom([user]);
+    }
+  };
+
+
+  const createChatRoom = async(users) => {
+
+    const newChatRoom = await DataStore.save(new ChatRoom({newMessages : 0}));
+
+    const authUser  = await Auth.currentAuthenticatedUser();
+    const dbUser = await DataStore.query(User,authUser.attributes.sub);
+    if(dbUser){
+      await addUserToChatRoom(dbUser,newChatRoom)
+      
+    }
+    
+   
+   await Promise.all(users.map((user) => addUserToChatRoom(user,newChatRoom)
+   
+  ));
+   
+
+    navigation.navigate('ChatRoom',{ id: newChatRoom.id});
+
+
+
+
+
+  }
+
+  const saveGroup = async () => {
+    await createChatRoom(selectedUsers);
+  };
+
+
+
+
   // useEffect(() => {
   //  const fetchUsers = async() =>{
   //    const fetchedUsers = await DataStore.query(User);
@@ -21,15 +80,28 @@ export default function UsersScreen() {
   // }, []);
 
   return (
-    <View style = {styles.page}>
+    <SafeAreaView style = {styles.page}>
      <FlatList data = {users}
-     renderItem ={({item}) => <UserItem user ={item} />}
+     renderItem ={({item}) => <UserItem user={item}
+     onPress={() => onUserPress(item)}
+     isSelected={isNewGroup ? isUserSelected(item) : undefined}/>}
      showsVerticalScrollIndicator ={false}
+     ListHeaderComponent={() => (
+      <NewGroupButton onPress={() => setIsNewGroup(!isNewGroup)} />
+    )}
      />
+
+
+        {isNewGroup && <Pressable style={styles.button} onPress={saveGroup}>
+          <Text style={styles.buttonText}>
+            Save group ({selectedUsers.length})
+          </Text>
+        </Pressable>}
+    
       
 
       
-    </View>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
@@ -37,7 +109,18 @@ const styles = StyleSheet.create({
   page:{
     flexDirection:"column",
     padding:10,
-  }
+  },
+  button: {
+    backgroundColor: "#3777f0",
+    marginHorizontal: 10,
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
+  },
   
 });
 
